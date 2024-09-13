1a9bcca8d047fb28d3c39b2691928aed90559a06
luis-domingues <luisdomingues117@gmail.com>
2024-09-12 21:45:04 -0300

    feat: implement controller to list contacts

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcProject.Context;
+
+namespace MvcProject.Controllers;
+
+public class ContactController : Controller
+{
+    private readonly AgendaContext _context;
+
+    public ContactController(AgendaContext context)
+    {
+        _context = context;
+    }
+        
+    public IActionResult Index()
+    {
+        var contacts = _context.Contacts.ToList();
+        return View(contacts);
+    }
+}
