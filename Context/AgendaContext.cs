b4c377328af122764374b05896bacff2cfdb0e2c
luis-domingues <luisdomingues117@gmail.com>
2024-09-12 21:54:05 -0300

    feat: create AgendaContext class to manage contacts

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcProject.Models;
+
+namespace MvcProject.Context;
+
+public class AgendaContext : DbContext
+{
+    public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) {  }
+
+    public DbSet<Contact> Contacts;
+}
