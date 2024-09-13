41f0d319a18cd13b3f3c8e4cd52c2e13b723cf09
luis-domingues <luisdomingues117@gmail.com>
2024-09-12 21:50:23 -0300

    feat: create data model to represent a contact

---
+++ Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcProject.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Number { get; set; }
+    public bool IsActive { get; set; }
+}
